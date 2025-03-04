82f49ed08c17c35741fb5abb716d79afc25af28a
Lab1210 <oladejiomolabake14@gmail.com>
2025-03-04 20:33:45 +0100

    fees payment

--- schoolapp/app/Student/Fees-Payment/page.jsx
+++ schoolapp/app/Student/Fees-Payment/page.jsx
@@ -2,11 +2,20 @@
 import { UserProvider } from "@/app/Components/StudentDashBoard/context/UserProvider";
 import FeesPaymentItem from "@/app/Components/StudentDashBoard/Pages/FeesPaymentItem";
 import React from "react";
+import styles from "../../css/layout.module.css";
 
 const Fees_Payment = () => {
   return (
     <UserProvider>
-      <FeesPaymentItem />
+      <Suspense
+        fallback={
+          <div className={styles.loadingContainer}>
+            <div className={styles.spinner}></div> {/* New: Spinner element */}
+          </div>
+        }
+      >
+        <FeesPaymentItem />
+      </Suspense>
     </UserProvider>
   );
 };
